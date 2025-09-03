b7573cd8d7024ce4001af8b6daed4185f1b83d85
amir <amirmallaei@gmail.com>
2025-09-03 10:47:12 -0500

    docker and docker compose finished

--- config/config/settings.py
+++ config/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&gm)ae-g#q%=unvx_^zbjwo8xgch)-s(-i08p34v0(a3i$js&5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'soroosh_db',
+        'USER': 'soroosh',
+        'PASSWORD': 'soroosh#963',
+        'HOST': 'postgres_of_laptopstore',
+        'PORT': '5432'
     }
 }
 
